58517b3c80a37b44eb2a67322dbc3420ea9dc09f
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-04-05 11:28:28 -0700

    questions are changing, verified the quiz cound

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        ["Critical Role is a DnD Podcast", "True"],
+        ["Travis has played both a Barbaian and a Warlock", "True"],
+        ["Alura is the Queen we all want", "True"]
+    ]
+    var questionNumber = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUi()
     }
-
-
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        
+        userAnswer == quiz[questionNumber][1] ? print("correct") : print("InCorrect")
+        
+        if questionNumber == (quiz.count - 1) {
+            questionNumber = 0
+        } else {
+            questionNumber += 1
+        }
+        
+        //questionNumber == quiz.count ? questionNumber = 0 : questionNumber += 1
+        //questionNumber += 1
+        updateUi()
+    }
+    
+    func updateUi(){
+        questionLabel.text = quiz[questionNumber][0]
+    }
+    
 }
 
